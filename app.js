39aad695cbb3ec3cd7f8ffe24f794a5074c092af
udit <raiudit80@gmail.com>
2023-01-05 13:53:06 +0530

    deployment error

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ app.use(require('./routes/post'));
 app.use(require('./routes/user'));
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
+    
     const path = require('path')
-    app.get("*",(req,res)=>{
+    app.get("/",(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
